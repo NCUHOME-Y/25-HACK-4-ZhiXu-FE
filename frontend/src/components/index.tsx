6cd26a76b78b764bc22cee253cb485195a461748
LinFangkui <3909285203@qq.com>
2025-11-07 19:13:37 +0800

    🐳 chore: ui中添加大量组件，由compents的index.tsx统一导出

--- frontend/src/components/index.tsx
+++ frontend/src/components/index.tsx
@@ -1,2 +1,48 @@
 //导出所有组件，方便统一导入
-export { Button } from "./ui/button"
+export { Alert, AlertTitle, AlertDescription } from "./ui/alert"
+export {
+  AlertDialog,
+  AlertDialogPortal,
+  AlertDialogOverlay,
+  AlertDialogTrigger,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogFooter,
+  AlertDialogTitle,
+  AlertDialogDescription,
+  AlertDialogAction,
+  AlertDialogCancel,
+} from "./ui/alert-dialog"
+export { Button, buttonVariants } from "./ui/button"
+export {
+  ButtonGroup,
+  ButtonGroupSeparator,
+  ButtonGroupText,
+  buttonGroupVariants,
+} from "./ui/button-group"
+export { Calendar, CalendarDayButton } from "./ui/calendar"
+export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from "./ui/card"
+export { Checkbox } from "./ui/checkbox"
+export {
+  Dialog,
+  DialogPortal,
+  DialogOverlay,
+  DialogClose,
+  DialogTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogFooter,
+  DialogTitle,
+  DialogDescription,
+} from "./ui/dialog"
+export { Input } from "./ui/input"
+export { Label } from "./ui/label"
+export { Popover, PopoverTrigger, PopoverContent } from "./ui/popover"
+export { Progress } from "./ui/progress"
+export { Separator } from "./ui/separator"
+export { Skeleton } from "./ui/skeleton"
+export { Spinner } from "./ui/spinner"
+export { Toaster as Sonner } from "./ui/sonner"
+export { Textarea } from "./ui/textarea"
+export { Toggle, toggleVariants } from "./ui/toggle"
+export { ToggleGroup, ToggleGroupItem } from "./ui/toggle-group"
